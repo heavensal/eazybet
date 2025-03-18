c7272ff0c26a9563d9527dc7eeb2744baef876a9
Adam Lamouri <adamlamouri@1344.fr>
2025-03-18 23:58:01 +0100

    ameliorer le header

--- app/controllers/competitions_controller.rb
+++ app/controllers/competitions_controller.rb
@@ -1,7 +1,15 @@
 class CompetitionsController < ApplicationController
   skip_before_action :authenticate_user!
   def index
-    @competitions = Competition.includes(events: :odds)
+    @competitions = Rails.cache.fetch("competitions_with_events_odds_comments", expires_in: 10.minutes) do
+      Competition
+        .select("competitions.*, COUNT(comments.id) AS comments_count")
+        .includes(events: :odds)
+        .left_joins(events: :comments)
+        .where(events: { commence_time: Time.zone.now.. })
+        .group("competitions.id, events.id")
+        .order("events.commence_time ASC")
+    end
   end
 
   def show
