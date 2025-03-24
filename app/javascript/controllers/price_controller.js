f0ed8930c3275171ffd4187169a3364dc0ed6da6
Adam Lamouri <adamlamouri@1344.fr>
2025-03-24 17:57:48 +0100

    data controller price

--- app/javascript/controllers/price_controller.js
+++ app/javascript/controllers/price_controller.js
@@ -9,8 +9,13 @@ export default class extends Controller {
   }
 
   update() {
+    console.log("odd:", this.oddTarget.innerText);
+    console.log("stake:", this.stakeTarget.value);
+    console.log("coins:", this.coinsTarget.innerText);
+
+
     const odd = parseFloat(this.oddTarget.innerText);
-    const stake = parseFloat(this.stakeTarget.value);
+    const stake = parseFloat(this.stakeTarget.value) || 0;
     this.coinsTarget.innerText = (odd * stake).toFixed(2);
     this.diamondsTarget.innerText = (Math.ceil((odd * stake - stake)) / 1000).toFixed(4);
   }
