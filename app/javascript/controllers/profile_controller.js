30024591b16b7732273030b0f6504e11cd7afe2f
ibrahima barry <themaltezers@gmail.com>
2025-03-25 05:07:14 +0100

    modif modal por diamants et coins

---
+++ app/javascript/controllers/profile_controller.js
@@ -0,0 +1,12 @@
+import { Controller } from "@hotwired/stimulus";
+
+// connects with data-controller="price"
+export default class extends Controller {
+
+
+  connect() {
+    console.log('connected');
+
+  }
+
+}
